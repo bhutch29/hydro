969ad8bbf610d042fbec042ee97b96b9669ca487
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2024-04-24 09:10:09 -0700

    add hostname when SSH_CONNECTION is set

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,5 @@
 function fish_prompt --description Hydro
-    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
+    set --local host
+    set --query SSH_CONNECTION && set --local host " $hostname"
+    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal$host $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
 end
